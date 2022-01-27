2de81dcb31f55b35588aa2e76840bc970e8e9141
Adrian Komuda <adrian.komuda.programista@gmail.com>
2022-01-27 23:06:17 +0100

    OCP v 1.1 - after addition a new spel.

--- src/me/naioku/solid_article/Mage.java
+++ src/me/naioku/solid_article/Mage.java
@@ -9,6 +9,9 @@ public class Mage {
         } else if (spell instanceof Slow) {
             Slow slow = (Slow) spell;
             slow.cast();
+        } else if (spell instanceof DragonShout) {
+            DragonShout dragonShout = (DragonShout) spell;
+            dragonShout.cast();
         } else {
             System.out.println("No such spell!");
         }
